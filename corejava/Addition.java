910ece2bf5fd810fe6d53adce293854ed580fb82
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-01-29 11:09:36 +0530

    Addition methods

---
+++ corejava/Addition.java
@@ -0,0 +1,15 @@
+
+class Addition 
+{
+   void add(int a ,int b)
+   {
+       System.out.println(a+b);
+   }
+   public static void main(String[] args) {
+       Addition obj = new Addition();
+       obj.add(10 , 20);
+       obj.add(2, 4);
+   }
+
+}
+
--- corejava/Vehicle.java
+++ corejava/Vehicle.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Vehicle {
+    protected String brand = " ford";
+    public void honk() {
+        System.out.println("tooor,tooor!");
+    }
+}
+
+class Car extends Vehicle {
+    private String modelName = "Mustang";
+    public static void main(String [] args){
+
+        //Create a myCar object
+        Car myCar = new Car();
+        myCar.honk();
+        System.out.println(myCar.brand + " " +myCar.modelName);
+    }
 }
