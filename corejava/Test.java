05117bdc0a10b2beeb0d052894f3fd466f54586d
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-01-29 11:28:51 +0530

    without arguements and with return type

---
+++ corejava/Test.java
@@ -0,0 +1,13 @@
+class Test {
+  int display() {
+        return 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("start");
+        Test t = new Test();
+       int result = t.display();  
+
+        System.out.println(result);
+    }
+}
