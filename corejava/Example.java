f477235927e11a8d77f5caf5ba370e6af50b3f9a
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-01-29 11:35:44 +0530

    with arguement and with return type.

---
+++ corejava/Example.java
@@ -0,0 +1,13 @@
+ class Example {
+
+   int findSquare(int n)
+    {
+        return n*n;
+    }
+    public static void main(String[] args) {
+        Example e = new Example();
+
+    int  result = e.findSquare(5);
+       System.out.println(result);
+    }
+}
